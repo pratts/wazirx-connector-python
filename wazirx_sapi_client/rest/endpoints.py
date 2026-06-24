bfdc8f770ae600a2dd8d8294d58ad44331fdd9c2
Prateek Sharma <prateeksharma.2801@gmail.com>
2026-06-24 15:06:59 +0530

    Add missing Postman collection endpoints and fix system_status bug
    Adds klines, my_trades, coins, withdraw/withdraw_history,
    deposit_address, sub_account_transfer_history, sub_accounts
    and sub_account_fund_transfer to endpoints.py and api_mapper.json.
    
    Fixes system_status mapping which incorrectly pointed to the "time"
    endpoint

--- wazirx_sapi_client/rest/endpoints.py
+++ wazirx_sapi_client/rest/endpoints.py
@@ -10,7 +10,10 @@ ENDPOINTS = {
     "trades": 'v1/trades',
     "historical_trades": 'v1/historicalTrades',
 
+    "klines": 'v1/klines',
+
     # Account API Endpoints
+    "my_trades": 'v1/myTrades',
     "order": 'v1/order',
     "test_order": 'v1/order/test',
     "open_orders": 'v1/openOrders',
@@ -19,6 +22,16 @@ ENDPOINTS = {
     "funds": 'v1/funds',
 
     # WebSocket-Client
-    "create_auth_token": "v1/create_auth_token"
+    "create_auth_token": "v1/create_auth_token",
+
+    # Crypto SAPIs
+    "coins": "v1/coins",
+    "crypto_withdraws": "v1/crypto/withdraws",
+    "crypto_deposit_address": "v1/crypto/deposits/address",
+
+    # Sub-Account
+    "sub_account_transfer_history": "v1/sub_account/fund_transfer/history",
+    "sub_accounts": "v1/sub_account/accounts",
+    "sub_account_fund_transfer": "v1/sub_account/fund_transfer",
 
 }
